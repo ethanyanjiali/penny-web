10e674d3177e0d12ac589b643c7ab3a04d4b3db1
Ethan Yanjia Li <liyanjia92@gmail.com>
2018-02-17 00:17:35 -0800

    better locale

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import store from './store';
@@ -10,24 +11,30 @@ import zhCN from './i18n/zh-CN';
 import ru from './i18n/ru';
 import zhCNLocaleData from 'react-intl/locale-data/zh';
 import ruLocaleData from 'react-intl/locale-data/ru';
+import { API_HOST } from './config';
 
-addLocaleData([ ...zhCNLocaleData, ...ruLocaleData ]);
+addLocaleData([...zhCNLocaleData, ...ruLocaleData]);
 
-const getLocale = () => {
-	if (navigator == undefined) {
-		return 'en';
-	} else if (navigator.languages != undefined) {
-		return navigator.languages[0]; 
-	} else if (navigator.language != undefined) {
-		return navigator.language;
+const getLocale = (acceptLanguage) => {
+	if (_.isString(acceptLanguage)) {
+		return acceptLanguage && acceptLanguage.split(',') && acceptLanguage.split(',')[0]
+	} else if (window.navigator.languages != undefined) {
+		return window.navigator.languages[0];
+	} else if (window.navigator.language != undefined) {
+		return window.navigator.language;
+	} else if (window.navigator.userLanguage != undefined) {
+		return window.navigator.userLanguage;
+	} else if (window.navigator.systemLanguage != undefined) {
+		return window.navigator.systemLanguage;
 	}
 	return 'en';
 };
 
-const getTranslation = () => {
-	const locale = getLocale();
+const getTranslation = (locale) => {
 	switch (locale) {
 		case 'zh-CN':
+		case 'zh-cn':
+		case 'zh_cn':
 		case 'zh':
 			return zhCN;
 		case 'ru':
@@ -37,13 +44,19 @@ const getTranslation = () => {
 	}
 };
 
-ReactDOM.render(
-	<IntlProvider locale={ getLocale() } defaultLocale='en' messages={ getTranslation() }>
-		<Provider store={store}>
-			<Router history={browserHistory}>
-				{ routes }
-			</Router>
-		</Provider>
-	</IntlProvider>,
-  	document.getElementById('react-container')
-);
+fetch(`${API_HOST}common/lang`)
+	.then((response) => response.text())
+	.then((acceptLanguage) => {
+		const locale = getLocale(acceptLanguage);
+		console.log(locale);
+		ReactDOM.render(
+			<IntlProvider locale={ locale } defaultLocale='en' messages={getTranslation(locale)}>
+				<Provider store={store}>
+					<Router history={browserHistory}>
+						{routes}
+					</Router>
+				</Provider>
+			</IntlProvider>,
+			document.getElementById('react-container')
+		);
+	});
